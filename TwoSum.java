c442059b3d61b1a590c605d35aecd6c0e67feb8c
zhandamm <sai.eduself@gmail.com>
2021-10-27 22:48:32 +0300

    added TwoSumTask

---
+++ TwoSum.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class TwoSum {
+    public void twoSum(int[] nums, int target) {
+        int sum;
+
+        for (int i = 0; i <= nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                sum = nums[i] + nums[j];
+                if (sum == target) {
+                    nums = new int[]{i, j};
+                }
+            }
+        }
+        System.out.println(nums[0] + " " + nums[1]);
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+       twoSum.twoSum(new int[]{1,2,3,4}, 6);
+
+
+    }
+}
